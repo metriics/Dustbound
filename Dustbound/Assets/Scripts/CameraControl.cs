70664ec02239cf05c68c313a0e0f1d9fbf5c9f42
Steph <stephane.gagnon00@gmail.com>
2020-07-04 17:21:28 -0400

    Add base project

---
+++ Dustbound/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.InputSystem;
+
+public class CameraControl : MonoBehaviour
+{
+    private Input control;
+
+    [SerializeField]
+    private GameObject player;
+
+    private Vector3 targetPos;
+    private Vector3 offset = new Vector3(0.0f, 4.0f, -8.0f);
+    private Vector3 velocity = Vector3.zero;
+
+    //camera rotate
+    private Vector2 cam;
+    private float camSpeed = 90.0f;
+
+    void Awake()
+    {
+        control = new Input();
+
+        control.Gameplay.Camera.performed += ctx => cam = ctx.ReadValue<Vector2>();
+        control.Gameplay.Camera.canceled += ctx => cam = Vector2.zero;
+
+        transform.LookAt(player.transform.position);
+    }
+
+    void FixedUpdate()
+    {
+        //angle to rotate camera
+        Quaternion rotAngle = Quaternion.AngleAxis(cam.x * Time.deltaTime * camSpeed, Vector3.up);
+        offset = rotAngle * offset;
+
+        //smooth follow
+        targetPos = player.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, 0.01f);
+        transform.LookAt(player.transform);
+    }
+
+
+    private void OnEnable()
+    {
+        control.Enable();
+    }
+
+    private void OnDisable()
+    {
+        control.Disable();
+    }
+
+
+}
---
+++ Dustbound/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cam;
+
+    //movements
+    private Input control;
+    private Vector2 movement;
+    private float moveSpeed = 7.0f;
+    private Vector3 direction;
+
+    //dodge roll
+    private bool isDashing = false;
+    private float rollTimer = 0.0f;
+    private float rollCooldown = 1.0f;
+
+    //jump
+    private bool isGrounded;
+    [SerializeField]
+    private Rigidbody body;
+
+    private bool isJumping;
+    private float jumpForce = 7.0f;
+    RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        control = new Input();
+
+        //ctx = context, can be named anything; lambda expression
+        control.Gameplay.Move.performed += ctx => movement = ctx.ReadValue<Vector2>();
+        control.Gameplay.Move.canceled += ctx => movement = Vector2.zero;
+
+        control.Gameplay.Jump.performed += ctx => Jump();
+        control.Gameplay.DodgeRoll.performed += ctx => DodgeRoll();
+    }
+
+    void Update()
+    {
+        direction = new Vector3(movement.x, 0.0f, movement.y);
+        direction = cam.transform.TransformDirection(direction);
+        direction.y = 0.0f;
+
+        if (direction != Vector3.zero)
+        {
+            //immediate rotation
+            transform.rotation = Quaternion.LookRotation(direction);
+
+            //slow rotation
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction),
+            //0.01f);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+
+        body.MovePosition(transform.position + (direction * moveSpeed * Time.fixedDeltaTime));
+
+        if (isJumping)
+        {
+            body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isJumping = false;
+        }
+        
+        if (isDashing)
+        {
+            body.AddForce(transform.forward * 8.0f, ForceMode.Impulse);
+            rollTimer = Time.fixedTime + rollCooldown;
+            isDashing = false;
+        }
+   
+    }
+
+    void GroundCheck()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.0f)){
+            isGrounded = true;
+        } else
+        {
+            isGrounded = false;
+        }
+
+    }
+
+    void Jump()
+    {
+        GroundCheck();
+
+        if (isGrounded && !isDashing && Time.time > rollTimer) 
+        {
+            isJumping = true;
+        }
+    }
+
+    void DodgeRoll()
+    {
+        GroundCheck();
+
+        if (isGrounded && !isJumping && !isDashing && Time.time > rollTimer)
+        {
+            isDashing = true;
+        }
+    }
+
+
+    private void OnEnable()
+    {
+        control.Enable();
+    }
+
+    private void OnDisable()
+    {
+        control.Disable();
+    }
+
+
+}
