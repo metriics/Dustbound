efd38a1765a230e3c7a34898ffe1288238c911d1
Hunter <hunt3rchu@gmail.om>
2020-07-10 00:34:49 -0400

    [changes] file management

---
+++ Dustbound/Assets/Scripts/Asset Scripts/Character.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Character : ScriptableObject
+{
+    public Who who;
+    public string charName;
+    public string charDesc;
+    public int baseHealth;
+    public int baseDmg;
+    public int baseArmour;
+
+    public enum Who { player, enemy, npc }
+}
---
+++ Dustbound/Assets/Scripts/Character Scripts/Behaviour Scripts/BaseBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseBehaviour : MonoBehaviour
+{
+    public CharacterState state;
+    public Character character;
+
+    public enum CharacterState { idle, attacking, moving }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == CharacterState.idle)
+        {
+            idle();
+        }
+        else if(state == CharacterState.attacking)
+        {
+            attacking();
+        }
+        else if(state == CharacterState.moving)
+        {
+            moving();
+        }
+    }
+
+    virtual public void idle()
+    {
+        //Behaviour for idle position
+        Debug.Log(character.charName + " is idle");
+    }
+
+    virtual public void attacking()
+    {
+        //Behaviour for attacking position
+        Debug.Log(character.charName + " is attacking");
+    }
+
+    virtual public void moving()
+    {
+        //Behaviour for moving position
+        Debug.Log(character.charName + " is moving");
+    }
+}
