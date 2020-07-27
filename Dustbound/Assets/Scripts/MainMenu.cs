e4d303f7ad0c003b66f209c991874def56423e09
Steph <stephane.gagnon00@gmail.com>
2020-07-27 18:30:43 -0400

    Main menu added w/ scene switch

---
+++ Dustbound/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    string playerName;
+    [SerializeField] public TMPro.TMP_InputField inputField;
+
+    public void UpdateName()
+    {
+        playerName = inputField.text;
+        Debug.Log(playerName);
+    }
+
+    public void OnPlayPressed()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+}
