a080fd20ff4ca93b7b9b8bd252537d01a38bbcf9
Jonah Griffin <jonah.griffin@uoit.net>
2020-09-29 16:02:03 -0400

    Main Menu Working/Pause menu WIP

---
+++ Dustbound/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+
+    public void PlayGame(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+        
+    }
+
+    public void MainMenu(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+
+    public void Resume(){
+        //Resume Code Goes in here
+    }
+    public void Options(){
+        //Option Code Goes in here
+    }
+}
